b9104e561755ea8146006a4b468ed6da75d15da5
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 14:52:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(s)
   words = s.split(" ")
-  words.collect {|w| w.reverse}.join(" ")
+  puts words.collect {|w| w.reverse}.join(" ")
 end 
 
 s = "Hello there, and how are you?"
