55078ff0c8b030bcfcdbcf8671de22e3760e1602
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 14:52:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,4 @@
 def reverse_each_word(s)
-  # words = s.split(" ")
-  # reversed = []
-  # words.each do |w|
-  #   reversed << w.reverse 
-  # end 
-  # s = reversed.join(" ")
-  # puts s
   words = s.split(" ")
   words.collect {|w| w.reverse}.join(" ")
 end 
